42d67d938d0fceffea1028a01f75d6b498705039
Fares-sghaier# <faroussassg007@gmail.com>
2022-04-28 18:10:46 +0100

    Testing the routes on 3 components

--- Front-star-buy/src/app/app-routing.module.ts
+++ Front-star-buy/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaymentComponent } from './payment/payment.component';
+import { ReviewListComponent } from './review-list/review-list.component';
+import { ReviewComponent } from './review/review.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:PaymentComponent,path:""},
+  {component:ReviewComponent,path:"review"},
+  {component:ReviewListComponent,path:"review-list"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-star-buy/src/app/app.module.ts
+++ Front-star-buy/src/app/app.module.ts
@@ -7,14 +7,13 @@ import { ReviewComponent } from './review/review.component';
 import { ReviewListComponent } from './review-list/review-list.component';
 import { PaymentComponent } from './payment/payment.component';
 import { PostsComponent } from './posts/posts.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     ReviewComponent,
     ReviewListComponent,
     PaymentComponent,
-    PostsComponent
+    PostsComponent,
   ],
   imports: [
     BrowserModule,
