f7abec61994033aa954d91203f26dc82412eb12b
Fares-sghaier# <faroussassg007@gmail.com>
2022-04-28 16:42:24 +0100

    creating 4 components

--- Front-star-buy/src/app/app.module.ts
+++ Front-star-buy/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReviewComponent } from './review/review.component';
+import { ReviewListComponent } from './review-list/review-list.component';
+import { PaymentComponent } from './payment/payment.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReviewComponent,
+    ReviewListComponent,
+    PaymentComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
