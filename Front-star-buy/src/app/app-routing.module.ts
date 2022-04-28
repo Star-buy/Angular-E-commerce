ed5e7fd4d86c6125de5214462384225a50788b72
nounou NOUNOU <brathebow@gmail.com>
2022-04-28 23:17:42 +0100

    first commit setting 4 components and 4 routes

--- Front-star-buy/src/app/app-routing.module.ts
+++ Front-star-buy/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AccessoriesComponent } from './accessories/accessories.component';
+import { AdminDataComponent } from './admin-data/admin-data.component';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
+import { CardComponent } from './card/card.component';
+
+
+const routes: Routes = [
+  {component:AccessoriesComponent, path: "accessories"},
+  {component:AdminDataComponent, path: "admin-data"},
+  {component:AdminProductsComponent, path: "admin-products"},
+  {component:CardComponent, path: "card"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-star-buy/src/app/app.module.ts
+++ Front-star-buy/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccessoriesComponent } from './accessories/accessories.component';
+import { AdminDataComponent } from './admin-data/admin-data.component';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessoriesComponent,
+    AdminDataComponent,
+    AdminProductsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
