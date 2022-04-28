bb2426acfddbc824ae109e3d2df7a8d10fbae99e
unknown <sirinedakhli01@gmail.com>
2022-04-29 01:04:43 +0200

    setting nav components +routes

--- Front-star-buy/src/app/app-routing.module.ts
+++ Front-star-buy/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AdminPartComponent} from './adminPart/adminPart.component';
+import {SigninComponent} from './signin/signin.component';
+import {SignupComponent} from './signup/signup.component';
+import {UnderNavComponent} from './underNav/underNav.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {component:AdminPartComponent, path:"adminPart"},
+  {component:SigninComponent, path:"signin"},
+  {component:SignupComponent, path:"signup"},
+  {component:UnderNavComponent, path:"underNavComponent"},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-star-buy/src/app/app.module.ts
+++ Front-star-buy/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { UnderNavComponent } from './underNav/underNav.component';
+import { AdminPartComponent } from './adminPart/adminPart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    UnderNavComponent,
+    AdminPartComponent
   ],
   imports: [
     BrowserModule,
